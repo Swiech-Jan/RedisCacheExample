cdc6f6f241237917abb7c9e82644d9fddfc440f6
Swiech-Jan <semper666@gmail.com>
2022-06-05 01:10:11 +0200

    new endpoint

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(
 
 var port = 3000;
 
+//------------------------- PHOTOS ------------------------
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
   const { data } = await axios.get(
@@ -19,6 +20,18 @@ app.get("/photos", async (req, res) => {
   );
   res.json(data);
 });
+
+//----- REDIS
+
+//-----
+
+app.get("/photos/:id", async (req, res) => {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+  );
+  res.json(data);
+});
+//----------------------------------------------------------
 
 app.listen(port, function () {
   console.log("Example app listening on port " + port + "!");
